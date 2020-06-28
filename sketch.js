36c22ce9a828a124579b24cc7520232ed964fea9
SWAPNILXYZ <66053332+SWAPNILXYZ@users.noreply.github.com>
2020-06-28 11:56:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,69 @@
-function setup() {
-  createCanvas(1600,400);
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-  bullet=createSprite(50, 200, 100, 10);
-  bullet.shapeColor="white";
-  wall=createSprite(1200,200,thickness,height/2);
-  wall.shapeColor=(80,80,80);
-
-  speed=random(223,321);
-  weight=random(30,52);
-  thickness=random(22,83);
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
-var bullet,wall;
-var speed,weight,thickness;
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	
+
+	packageSprite=createSprite(width/5, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:.7, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
 
 function draw() {
-  background(0,0,0); 
-  bullet.velocityX=speed; 
-  
-  if (hasColiided(bullet,wall)){
-    bullet.velocityX=0;
-    var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
-
-    if (damage<10){
-      bullet.shapeColor="green";
-    }
-    if (damage>10){
-      bullet.shapeColor="red";
-    }
-  }
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
   drawSprites();
+ 
 }
-function hasColiided (bullet,wall){
- bulletRightEdge=bullet.x+bullet.width;
- wallLeftEdge=wall.x;
 
- if (bulletRightEdge>=wallLeftEdge){
-   return true;
- }
- return false;
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    
+  }
 }
+
+
+
